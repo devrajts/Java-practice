cc59a49de0ee17cc7a4133f167a3866287b1b0ef
Devaraj T S <103634904+devrajts@users.noreply.github.com>
2022-09-06 10:54:28 +0530

    Add files via upload

---
+++ infy/assignment-day-2/cricketscore.java
@@ -0,0 +1,13 @@
+public class cricketscore {
+    public static void main(String[] args) {
+        int singles=15;
+        int doubles=7;
+        int thribles=2;
+        int four=7;
+        int six=4;
+
+        int total_score=(singles*1)+(doubles*2)+(thribles*3)+(four*4)+(six*6);
+
+        System.out.println(total_score);
+    }
+}
