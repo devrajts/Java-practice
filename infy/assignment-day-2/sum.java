f284c9d82203ac2ead587ca71e4205096fc285d2
Devaraj T S <103634904+devrajts@users.noreply.github.com>
2022-09-06 10:55:20 +0530

    Add files via upload

---
+++ infy/assignment-day-2/sum.java
@@ -0,0 +1,12 @@
+public class sum {
+        public static void main(String[] args) {
+            int num = 10, count = 1, total = 0;
+            while(count <= num){
+                total = total + count;
+                count++;
+            }
+
+            System.out.println(total);
+        }
+
+}
