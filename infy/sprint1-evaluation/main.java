c4c2dc09a03a67b2f21cae4be9c56371edde8fb9
Devaraj T S <103634904+devrajts@users.noreply.github.com>
2022-09-09 19:47:42 +0530

    Add files via upload

---
+++ infy/sprint1-evaluation/main.java
@@ -0,0 +1,37 @@
+public class main {
+      static void printMonths(String month){
+        if (month=="jan"){
+            System.out.println("This is the first month of the year January");
+        } else if (month=="feb") {
+            System.out.println("This is the second month of the year February");
+        } else if (month=="mar") {
+            System.out.println("This is the third month of the year March");
+        } else if (month=="apr") {
+            System.out.println("This is the fourth month of the year April");
+        } else if (month=="may") {
+            System.out.println("This is the fifth month of the year May");
+        }else if (month=="jun") {
+            System.out.println("This is the sixth month of the year June");
+        }else if (month=="jul") {
+            System.out.println("This is the seventh month of the year July");
+        }else if (month=="aug") {
+            System.out.println("This is the eighth month of the year August");
+        }else if (month=="sep") {
+            System.out.println("This is the ninth month of the year September");
+        }else if (month=="oct") {
+            System.out.println("This is the tenth month of the year October");
+        }else if (month=="nov") {
+            System.out.println("This is the eleventh month of the year November");
+        }else if (month=="dec") {
+            System.out.println("This is the twelveth month of the year December");
+        }
+    }
+
+    public static void main(String[] args) {
+        printMonths("jun");
+    }
+
+
+
+
+}
