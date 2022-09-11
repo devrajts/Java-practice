026859786ab1ed25259850203d045588ee6f440a
Devaraj T S <103634904+devrajts@users.noreply.github.com>
2022-09-11 17:42:49 +0530

    Add files via upload

--- infy/Practice/Main.java
+++ infy/Practice/Main.java
@@ -1,6 +1,23 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello JA111 Infosys Batch");
-        System.out.println("hello");
+    static int j=200;
+//    static A a1 = new A();
+    public static void main(String[] args)
+    {
+        Main obj=new Main();
+        System.out.println(obj.j);
+        A a1 = new A();
+//System.out.println(obj.j);// 200
+//System.out.println(obj.a1); //A@23232
+//obj.a1.funA();
+        a1.funA();
+//        Main.a1.funA();
+//System.out.println();
     }
-}`
+
+}
+ class A {
+    int x = 10;
+    void funA(){
+        System.out.println("inside funA of A class");
+    }
+}
