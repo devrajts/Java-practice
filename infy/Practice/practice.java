0b79427c3fe5b8e7679c51ec70fa5a9a76b3136a
Devaraj T S <103634904+devrajts@users.noreply.github.com>
2022-09-12 16:38:20 +0530

    Add files via upload

---
+++ infy/Practice/practice.java
@@ -0,0 +1,10 @@
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("Name : Devaraj T S");
+        System.out.println("Father Name : Shivalingappa T D");
+        System.out.println("Mother Name :  Latha T S");
+        System.out.println("Age : 24");
+        System.out.println("Gender : Male");
+        System.out.println("Mobile No : 8105848723");
+    }
+}
